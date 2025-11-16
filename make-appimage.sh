ae521a34a03f1793f0370ff492c6ec70f8f99425
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-16 10:20:40 +0100

    Migrate to new generic AppImage template

---
+++ make-appimage.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+VERSION=$(pacman -Q identity | awk '{print $2; exit}')
+export ARCH VERSION
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.bg.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=/usr/share/icons/hicolor/scalable/apps/org.gnome.gitlab.YaLTeR.Identity.svg
+export DESKTOP=/usr/share/applications/org.gnome.gitlab.YaLTeR.Identity.desktop
+export DEPLOY_OPENGL=1
+export DEPLOY_GSTREAMER=1
+export STARTUPWMCLASS=identity # For Wayland, this is 'org.gnome.gitlab.YaLTeR.Identity', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
+
+# Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
+quick-sharun /usr/bin/identity
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
