fd2c7f11c780c5826dc8f7a809302d8f819a2529
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-20 19:33:49 +0100

    OpenGL is automatically deployed now

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,6 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.gnome.gitlab.YaLTeR.Identity.svg
 export DESKTOP=/usr/share/applications/org.gnome.gitlab.YaLTeR.Identity.desktop
-export DEPLOY_OPENGL=1
 export DEPLOY_GSTREAMER=1
 export STARTUPWMCLASS=org.gnome.gitlab.YaLTeR.Identity # Default to Wayland's wmclass. For X11, GTK_CLASS_FIX will force the wmclass to be the Wayland one.
 export GTK_CLASS_FIX=1
